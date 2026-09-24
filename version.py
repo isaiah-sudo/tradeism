fa63644138e767939729e383cf8df3db05e7eb3e
Isaiah <timmerisaiuah@gmail.com>
2026-09-23 18:43:27 -0700

    chore: point GITHUB_REPO to updated tradeism repo name

--- version.py
+++ version.py
@@ -4,4 +4,4 @@ Day Trading Simulator version and metadata specification.
 
 __version__ = "1.3.0"
 APP_NAME = "Day Trading Simulator"
-GITHUB_REPO = "isaiah-sudo/daytradesim"
+GITHUB_REPO = "isaiah-sudo/tradeism"
